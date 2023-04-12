b3557ba8c101671d2a03b7eeac1eac2b59d3a6ae
MDzaja <mirkodzaja0@gmail.com>
2023-04-12 11:03:19 +0100

    chnaged project name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "electric_vehicles.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "everyday_cooking.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
